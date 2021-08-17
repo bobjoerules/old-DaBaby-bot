4b503825c09555ce46fdf28acb40a0efd39b5ed0
Bobjoerules <39201587+bobjoerules@users.noreply.github.com>
2021-08-17 06:00:59 +0000

    deleted this website

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 server.all('/', (req, res)=>{
-    res.send('Bot website has been moved to:')
+    res.send('Bot website has been moved to: <a href="https://bobjoerules.github.io/DaBabyBot/">https://bobjoerules.github.io/DaBabyBot/</a>')
 })
 function keepAlive(){
     server.listen(3000, ()=>{console.log("Website and on forever!")});
